22b61c2341d782f871dd3a5f44657d776d62af48
Niraj Aryal <stranzerneeraz12@gmail.com>
2021-06-20 16:51:43 +0545

    Update ApplicationConstants.java

--- src/constants/ApplicationConstants.java
+++ src/constants/ApplicationConstants.java
@@ -5,7 +5,7 @@ public class ApplicationConstants {
     public static String DRIVER_NAME = "com.mysql.jdbc.Driver";
     public static String DB_NAME = "lagani";
     public static  final String DB_USERNAME = "root";
-    public static final String  DB_PASSWORD = "stranzer12@";
+    public static final String  DB_PASSWORD = "";
     public static final String DB_URL = "jdbc:mysql://localhost:3306/lagani?autoReconnect=true&useSSL=false";
     public static final  int DB_PORT = 3306;
 
